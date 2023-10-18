9476f1f5b29a51e125cfac220e96c4a7c1b56640
marcos-davi <77940812+marcos-davi@users.noreply.github.com>
2023-10-19 00:23:42 +0200

    add citas

--- CITAS-ALEATORIAS/citas.js
+++ CITAS-ALEATORIAS/citas.js
@@ -1,42 +1,75 @@
-let citas = [
-    {
-      'autor': 'Albert Einstein',
-      'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
-    },
-    {
-      'autor': 'Anónimo',
-      'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
-    },
-    {
-      'autor': 'Alan Kay',
-      'texto': 'La mejor forma de predecir el futuro es inventarlo.'
-    },
-    {
-      'autor': 'Amelia Earhart',
-      'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
-    },
-    {
-      'autor': 'Aristotle',
-      'texto': 'La calidad no es un acto, es un hábito.'
-    },
-    {
-      'autor': 'Benjamin Franklin',
-      'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
-    },
-    {
-      'autor': 'Charles R. Swindoll',
-      'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
-    },
-    {
-      'autor': 'Jane Goodall',
-      'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
-    },
-    {
-      'autor': 'John Muir',
-      'texto': 'El poder de la imaginación nos hace infinitos.'
-    },
-    {
-      'autor': 'Mark Twain',
-      'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
-    }
-  ];
+    let citas = [
+  {
+    'autor': 'Albert Einstein',
+    'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+  },
+  {
+    'autor': 'Anónimo',
+    'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+  },
+  {
+    'autor': 'Alan Kay',
+    'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+  },
+  {
+    'autor': 'Amelia Earhart',
+    'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+  },
+  {
+    'autor': 'Aristotle',
+    'texto': 'La calidad no es un acto, es un hábito.'
+  },
+  {
+    'autor': 'Benjamin Franklin',
+    'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+  },
+  {
+    'autor': 'Charles R. Swindoll',
+    'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+  },
+  {
+    'autor': 'Jane Goodall',
+    'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+  },
+  {
+    'autor': 'John Muir',
+    'texto': 'El poder de la imaginación nos hace infinitos.'
+  },
+  {
+    'autor': 'Mark Twain',
+    'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+  },
+  {
+    'autor': 'Lao Tsé',
+    'texto': 'No hay que ir para atrás ni para darse impulso.'
+  },
+  {
+    'autor': 'Mahatma Gandhi',
+    'texto': 'No hay caminos para la paz; la paz es el camino.'
+  },
+  {
+    'autor': 'Steve Jobs',
+    'texto': 'El único modo de hacer un gran trabajo es amar lo que haces.'
+  },
+  {
+    'autor': 'Confucio',
+    'texto': 'Escoge un trabajo que te guste, y nunca tendrás que trabajar ni un solo día de tu vida.'
+  },
+  {
+    'autor': 'Zig Ziglar',
+    'texto': 'A veces la adversidad es lo que necesitas encarar para ser exitoso.'
+  },
+  {
+    'autor': 'Muhammad Ali',
+    'texto': 'No cuentes los días, haz que los días cuenten.'
+  },
+  {
+    'autor': 'Walt Disney',
+    'texto': 'Piensa, sueña, cree y atrévete.'
+  },
+  {
+    'autor': 'Proverbio chino',
+    'texto': 'Si te caes siete veces, levántate ocho.'
+  }
+
+];
