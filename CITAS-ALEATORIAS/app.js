c2e33a61e8f3712b2a749e80d50123fcc89512e5
marcos-davi <77940812+marcos-davi@users.noreply.github.com>
2023-10-18 23:31:41 +0200

    CITAS-ALEATORIAS

---
+++ CITAS-ALEATORIAS/app.js
@@ -0,0 +1,16 @@
+let botonElem = document.getElementById('boton-cambiar-cita');
+let citaElem = document.getElementById('cita');
+let autorElem = document.getElementById('autor');
+
+function generarEnteroAleatorio(min, max) {
+// Sin incluir en los valores posibles.
+return Math.floor(Math.random() * (max - min) + min);
+}
+
+function cambiarCita() {
+  let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+  citaElem.innerText = `"${citas[indiceAleatorio].texto}"`;
+  autorElem.innerText = citas[indiceAleatorio].autor;
+}
+document.addEventListener('click', cambiarCita);
+cambiarCita();
---
+++ README.md
@@ -0,0 +1,3 @@
+## Link del curso
+
+[JavaScript - DOM](https://www.youtube.com/watch?v=koiPxFFiqJ4&list=PL4ONm-ifcbQIGjBqCnF_r02e2K-SyYgv2&index=7)
